f93f31209a2e7ae70f52b05ed3b216d87a0e2ac9
rozapa1998 <rodrigo1998.rz@gmail.com>
2020-12-15 02:19:38 -0300

    cambios de notaciones

--- Triple X/TripleX.cpp
+++ Triple X/TripleX.cpp
@@ -2,24 +2,31 @@
 
 int main()
 {
-    std::cout <<"Sistema de intrusion exitoso...";
+    //Strings de Bienvenida
+    std::cout <<"//Sistema de intrusion exitoso...";
     std::cout << std::endl;
-    std::cout << "Ingresando al sistema...";
+    std::cout << "//Ingresando al sistema...";
     std::cout << std::endl;
-    std::cout << "[ERROR]...";
+    std::cout << "//[ERROR]...";
     std::cout << std::endl;
-    std::cout << "Para iniciar al sistema debe ingresar clave de 3 dijitos...";
+    std::cout << "//Para iniciar al sistema debe ingresar clave de 3 dijitos...";
 
-    const int a = 4;
-    const int b = 8;
-    const int c = 7;
+    //Declaracion de variables
+    const int CodeA = 2;
+    const int CodeB = 3;
+    const int CodeC = 7;
 
-    const int sum = a+b+c;
-    const int mult = a*b*c;
+    //Suma y multiplicacion de variables
+    const int CodeSum = CodeA+CodeB+CodeC;
+    const int CodeMult = CodeA*CodeB*CodeC;
 
     std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << mult << std::endl;
+    std::cout << std::endl;
+    std::cout << "//PROGRAMA DE INTRUCION TRIPLEX CODIFICANDO..."<< std::endl;
+    std::cout << "+ Ea suma del codigo es igual a: "<< CodeSum << std::endl;
+    std::cout << "+ El producto del codigo es igual a: " << CodeMult << std::endl;
+
+    int PlayerGuess;
     
     
     return 0;
