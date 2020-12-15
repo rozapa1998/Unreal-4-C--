bf4205ebb36c28374845f8697a33aca79891615c
rozapa1998 <rodrigo1998.rz@gmail.com>
2020-12-15 01:45:10 -0300

    first commit

---
+++ Triple X/TripleX.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    std::cout <<"Sistema de intrusion exitoso...";
+    std::cout << std::endl;
+    std::cout << "Ingresando al sistema...";
+    std::cout << std::endl;
+    std::cout << "[ERROR]...";
+    std::cout << std::endl;
+    std::cout << "Para iniciar al sistema debe ingresar clave de 3 dijitos...";
+
+    const int a = 4;
+    const int b = 8;
+    const int c = 7;
+
+    const int sum = a+b+c;
+    const int mult = a*b*c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << mult << std::endl;
+    
+    
+    return 0;
+}
